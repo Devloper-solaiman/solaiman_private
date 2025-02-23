7f9c3a7932b39aeb37c96e4a4c2c50cd8671f94b
Devloper-solaiman <devlopersolaiman@gmail.com>
2025-02-23 12:30:46 +0600

    first commit

--- hooks/skills.hook.ts
+++ hooks/skills.hook.ts
@@ -1,9 +1,8 @@
-import { useSearchParams } from "next/navigation";
+/* eslint-disable prettier/prettier */
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
 
-import { SkillCategory } from "@/constants/skills.constants";
 import { TUpdateData } from "@/types";
 import {
   createSkill,
